1dcf473d4b9ebced056ab41f17b33d7ab685e811
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-04 10:53:10 +0300

    корректировка условий в блок-схеме

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int RandomArray(int[] array) // заполение массива положительными трехзначными числами
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+int PositiveNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if array[i]
+    }
+}
+
+int[] array = new(5);
+RandomArray(array);
