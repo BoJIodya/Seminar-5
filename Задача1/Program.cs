24f4772c89e72ed9f00d44c209d4f2a15b0fa3ce
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-04 11:13:43 +0300

    task ready

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,18 +1,35 @@
-﻿int RandomArray(int[] array) // заполение массива положительными трехзначными числами
+﻿void RandomArray(int[] array) // заполение массива положительными трехзначными числами
 {
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(100, 1000);
     }
-    return array;
+    //return array;
 }
-int PositiveNumbers(int[] array)
+
+void PrintArray(int[] array) // печать массива, для проверки
 {
     for (int i = 0; i < array.Length; i++)
     {
-        if array[i]
+        Console.Write($" {array[i]}");
     }
 }
 
-int[] array = new(5);
+int PositiveNumbers(int[] array) // подсчет четных элементов
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int[] array = new int[5];
 RandomArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(PositiveNumbers(array));
